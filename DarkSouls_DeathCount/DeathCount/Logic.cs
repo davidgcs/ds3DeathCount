5cd875ddfb1be48d770d218d376bfa36f6be91b4
Alexander Poplavsky <alexander.poplavsky@mmorthosoft.de>
2017-04-11 15:52:38 +0200

    Revert "Revert "- git module added: ProcessMemory - added DeathAddress with current pointer into Logic-Class""
    This reverts commit 649150445ebeda372a11d9db17382822ca8287e6.

---
+++ DarkSouls_DeathCount/DeathCount/Logic.cs
@@ -0,0 +1,32 @@
+﻿using IgroGadgets.Memory;
+using System;
+
+namespace DarkSouls_DeathCount.DeathCount
+{
+    class Logic
+    {
+        private ProcessMemory ProcessMemory { get; set; }
+        public IntPtr DeathAddress { get; private set; }
+
+        public Logic()
+        {
+            try
+            {
+                ProcessMemory = new ProcessMemory("DarkSoulsIII");
+                ReadBasePointer();
+            }
+            catch (Exception e)
+            {
+                // ToDo bring NoAdminPrivilegesException and NoProcessFoundException to user. As MessageBox for example
+                throw e;
+            }
+            
+        }
+
+        public void ReadBasePointer()
+        {
+            DeathAddress = ProcessMemory.ReadPointer64(new IntPtr(0x14473A818), new int[] { 0x98 });
+            Console.WriteLine(ProcessMemory.ReadMemoryInt(DeathAddress));
+        }
+    }
+}
